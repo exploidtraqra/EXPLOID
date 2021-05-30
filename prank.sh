d05e4821cffdf709b46ceeffd904dccbb1534526
DEVL_NOOB <69730462+yunanaexploit@users.noreply.github.com>
2021-05-30 09:42:20 +0000

    Create prank.sh

---
+++ prank.sh
@@ -0,0 +1,21 @@
+GNU nano 5.7                EXPLOID.sh
+#!/bin/bash
+clear
+naga(){
+echo """×××××××××××××××××××××××××××××××××××××××
+×        FAKE          HACK           ×
+× EXPLOIT LODEER XSPAM ERROR TO TARGET×
+×××××××××××××××××××××××××××××××××××××××
+"""
+}
+naga
+read -p 'enter target: ' file
+read -p 'enter spam virus int: ' j
+main="automain"
+i=0
+while [ $i -lt $j ]
+do
+sleep 0.1
+        echo -e  "\e[32m" $i $i  $i  $i "/data/\e[41mlodeer\e[0m/e>
+        ((i+=2))
+done
